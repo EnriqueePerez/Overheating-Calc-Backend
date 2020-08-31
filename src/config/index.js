d8dacd28d23ee2f3a92020719ec2faf0c20ec900
EnriqueePerez <enrike_084@hotmail.com>
2020-08-31 15:22:28 -0500

    Added .env example file

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const config = {
   dev: process.env.NODE_ENV,
   port: process.env.PORT,
-  cors: process.env.CORS,
   dbHost: process.env.DB_HOST,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
